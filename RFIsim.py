0f1a0239c45ae9c529c3dfc9a8d7ab7f1a52c06a
Chris Finlay <cfinlay@ska.ac.za>
2018-11-02 13:45:02 +0000

    Input and output now saved into an h5 file.

--- RFIsim.py
+++ RFIsim.py
@@ -1,3 +1,4 @@
+import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
@@ -49,10 +50,8 @@ class CustomSourceProvider(SourceProvider):
     def point_stokes(self, context):
         (lp, up), (lt, ut), (lc, uc) = context.dim_extents('npsrc', 'ntime', 'nchan')
         # (npsrc, ntime, nchan, 4)
-        s = np.asarray(lm_stokes, dtype=np.float64)[lp:up,None,None,:]
-        ft = source_spectrogram
-        
-        return s*ft
+
+        return spectra
     
     def direction_independent_effects(self, context):
         # (ntime, na, nchan, npol)
@@ -122,7 +121,7 @@ slvr_cfg = montblanc.rime_solver_cfg(mem_budget=6*1024*1024*1024,
 target_ra = 21.4439
 target_dec = -30.713199999999997
 target_ra, target_dec = 0., 0.
-tracking_hours = 0.5
+tracking_hours = 0.1
 integration_secs = 8
 obs_date = '2018/10/18'
 
@@ -161,17 +160,33 @@ source_spectrogram[2,:,1600:1750,0] = (67e3*np.random.rand()+67e3)*np.ones((1, 1
 
 lm = get_lm_tracks(target_ra, target_dec, transit, tracking_hours, integration_secs)
 
-###
-# Still need lm coords of satellite
-###
+astro_lm = (0.0, 0.0)
+
+# Set number of channels and antennas
 
 ntime = 1
 nchan = 4096
 na = 64
 
 bandpass = np.load('utils/bandpass/MeerKAT_Bandpass_HH-HV-VH-VV.npy').astype(np.complex128)
-
 FITSfiles = 'utils/beam_sim/beams/FAKE_$(corr)_$(reim).fits'
+
+# Set file name to save data to
+
+save_file = 'ra=' + str(target_ra) + '_dec=' + str(target_dec) + '_int_secs=' + \
+            str(integration_secs) + '_track-time=' + str(round(tracking_hours, 1)) + 'hrs_nants=' + \
+            str(nant) + '_nchan=' + str(nchan) + '.h5'
+
+
+# Need to change the number of sources variably
+with h5py.File(save_file, 'a') as fp:
+    fp['/input/lm'] = np.concatenate((np.ones((len(lm), 1, 2))*astro_lm, lm), axis=1)
+    fp['/input/UVW'] = UVW
+    fp['/input/A1'] = A1
+    fp['/input/A2'] = A2
+    fp['/input/bandpass'] = bandpass
+
+# Create visibilities array
 
 vis = np.zeros(shape=(len(lm), na*(na-1)/2, nchan, 4), dtype=np.complex128)
 
@@ -181,9 +196,30 @@ lm_stokes = [(1.0, 0.0, 0.0, 0.0),
              (1.0, 0.0, 1.0, 0.0),
             ]
 
+# Save input spectra
+s = np.asarray(lm_stokes, dtype=np.float64)[:,None,None,:]
+spectra = s*source_spectrogram
+
+with h5py.File(save_file, 'a') as fp:
+    fp['/input/spectra'] = spectra
+
+# Save input bandpasses
+np.random.seed(123)
+antenna_gains_auto = 200*np.random.rand(na) + 50
+antenna_gains_cross = 70*np.random.rand(na) + 10
+
+bandpass[:,:,:,[0,3]] *= antenna_gains_auto[None, :, None, None]
+bandpass[:,:,:,[1,2]] *= antenna_gains_cross[None, :, None, None]
+
+with h5py.File(save_file, 'a') as fp:
+    fp['/input/auto_pol_gains'] = antenna_gains_auto
+    fp['/input/cross_pol_gains'] = antenna_gains_cross
+
+
+
 for i in range(len(lm)):
     # LM coordinates
-    lm_coords = [(0, 0),
+    lm_coords = [astro_lm,
                  (lm[i,0,0], lm[i,0,1]),
                  (lm[i,1,0], lm[i,1,1]),
                 ]
@@ -203,4 +239,5 @@ for i in range(len(lm)):
                 sink_providers=sink_provs)
 
 # Save output
-np.save('vis.npy', vis)
+with h5py.File(save_file, 'a') as fp:
+    fp['/output/vis_dirty'] = vis
