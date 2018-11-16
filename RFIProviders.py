0ec2451997017f8bb42644273e5dd221c240671c
Chris Finlay <cfinlay@ska.ac.za>
2018-11-16 15:50:55 +0000

    Initial working version where source and sink providers have moved to RFIProviders.

---
+++ RFIProviders.py
@@ -0,0 +1,196 @@
+import numpy as np
+
+import montblanc
+from montblanc.impl.rime.tensorflow.sources import SourceProvider
+from montblanc.impl.rime.tensorflow.sinks import SinkProvider
+import montblanc.util as mbu
+
+
+from utils.sat_sim.sim_sat_paths import radec_to_lm
+
+######## Source Provider #######################################################
+
+class RFISourceProvider(SourceProvider):
+    """
+    Supplies data to montblanc via data source methods,
+    which have the following signature.
+
+    .. code-block:: python
+
+        def point_lm(self, context)
+            ...
+    """
+    def __init__(self, rfi_run, n_time, n_chan, n_ant, bandpass, gauss_sources,
+                 p_spectrum, rfi_lm, phase_centre, time_step, A1, A2, UVW):
+        self.rfi_run = rfi_run
+        self.n_time = int(n_time)
+        self.n_chan = n_chan
+        self.n_ant = n_ant
+        self.n_bl = n_ant*(n_ant-1)/2
+        self.UVW = UVW
+        self.bandpass = bandpass
+        self.gauss_sources = gauss_sources
+        self.n_gsrcs = len(gauss_sources)
+        self.p_spectrum = p_spectrum
+        self.n_rfi = p_spectrum.shape[0]
+        self.rfi_lm = rfi_lm
+        self.target = phase_centre
+        self.time_step = time_step
+        self.A1 = A1
+        self.A2 = A2
+
+
+    def name(self):
+        """ Name of Source Provider """
+        return self.__class__.__name__
+
+    def updated_dimensions(self):
+        """ Inform montblanc about dimension sizes """
+        dims = [("ntime", self.n_time),  # Timesteps
+                ("nchan", self.n_chan),  # Channels
+                ("na", self.n_ant),      # Antenna
+                ("nbl", self.n_bl),      # Baselines
+                ("ngsrc", self.n_gsrcs)]  # Number of astronomical sources
+        if self.rfi_run:
+            dims.append(("npsrc", self.n_rfi))
+        else:
+            dims.append(("npsrc", 0))
+
+        return dims
+
+    def point_lm(self, context):
+        """ Supply point source lm coordinates to montblanc """
+
+        # Shape (npsrc, 2)
+        (ls, us), _ = context.array_extents(context.name)
+
+        return np.asarray(self.rfi_lm, dtype=context.dtype)[ls:us, :]
+
+    def point_stokes(self, context):
+        extents = context.dim_extents('npsrc', 'ntime', 'nchan')
+        (lp, up), (lt, ut), (lc, uc) = extents
+        # (npsrc, ntime, nchan, 4)
+
+        spec = np.ones((1, self.n_time, 1, 1))
+        spec = self.p_spectrum*spec
+
+        return spec[lp:up, lt:ut, lc:uc, :]
+
+######## New ############################################################################
+
+    def gaussian_lm(self, context):
+        """ Returns an lm coordinate array to Montblanc. """
+
+        # lm = np.empty(context.shape, context.dtype)
+        lm = np.empty((self.n_gsrcs, 2), context.dtype)
+
+        # Get the extents of the time, baseline and chan dimension
+        (lg, ug) = context.dim_extents('ngsrc')
+
+        ra = self.gauss_sources['RA']
+        dec = self.gauss_sources['DEC']
+
+        lm[:,0], lm[:,1] = radec_to_lm(ra, dec, self.target)
+
+        return lm[lg:ug]
+
+    def gaussian_shape(self, context):
+        """ Returns a Gaussian shape array to Montblanc """
+
+        (lg, ug) = context.dim_extents('ngsrc')
+
+        emaj = np.deg2rad(self.gauss_sources['MajAxis'].values)
+        emin = np.deg2rad(self.gauss_sources['MinAxis'].values)
+        pa = np.deg2rad(self.gauss_sources['PA'].values)
+
+        # gauss = np.empty(context.shape, dtype=context.dtype)
+        gauss = np.empty((3, self.n_gsrcs), context.dtype)
+
+        gauss[0,:] = emaj * np.sin(pa)
+        gauss[1,:] = emaj * np.cos(pa)
+        emaj[emaj == 0.0] = 1.0
+        gauss[2,:] = emin / emaj
+
+        return gauss[:, lg:ug]
+
+    def gaussian_stokes(self, context):
+        """ Return a stokes parameter array to Montblanc """
+
+        # Get the extents of the time, baseline and chan dimension
+        extents = context.dim_extents('ngsrc', 'ntime', 'nchan')
+        (lg, ug), (lt, ut), (lc, uc) = extents
+
+        # stokes = np.empty(context.shape, context.dtype)
+        stokes = np.zeros((self.n_gsrcs, self.n_time, self.n_chan, 4),
+                           context.dtype)
+
+        stokes[:,:,:,0] = self.gauss_sources['Flux'].values[:,None,None]
+        stokes[:,:,:,1:] = 0.0
+
+        return stokes[lg:ug, lt:ut, lc:uc, :]
+
+######################################################################################
+
+    # def direction_independent_effects(self, context):
+    #     # (ntime, na, nchan, npol)
+    #     extents = context.dim_extents('ntime', 'na', 'nchan', 'npol')
+    #     (lt, ut), (la, ua), (lc, uc), (lp, up) = extents
+    #
+    #     bp = np.ones((self.n_time, 1, 1, 1))
+    #     bp = bandpass*bp
+    #     return bp[lt:ut, la:ua, lc:uc, lp:up]
+
+    def uvw(self, context):
+        """ Supply UVW antenna coordinates to montblanc """
+
+        # Shape (ntime, na, 3)
+        (lt, ut), (la, ua), (l, u) = context.array_extents(context.name)
+
+        if self.rfi_run:
+            idx = np.arange(self.time_step*self.n_bl,
+                           (self.time_step+1)*self.n_bl)
+            auvw = mbu.antenna_uvw(self.UVW[idx], self.A1, self.A2,
+                                   np.array([self.n_bl,]),
+                                   nr_of_antenna=self.n_ant)
+        else:
+            AA1 = np.array(self.n_time*list(self.A1))
+            AA2 = np.array(self.n_time*list(self.A2))
+            chunks = np.array(self.n_time*[self.n_bl], dtype=np.int)
+            auvw = mbu.antenna_uvw(self.UVW, AA1, AA2, chunks,
+                                   nr_of_antenna=self.n_ant)
+
+        return auvw[lt:ut, la:ua, l:u]
+
+######### Sink Provider ########################################################
+
+class RFISinkProvider(SinkProvider):
+    """
+    Receives data from montblanc via data sink methods,
+    which have the following signature
+
+    .. code-block:: python
+
+        def model_vis(self, context):
+            print context. data
+    """
+    def __init__(self, vis, rfi_run, time_step):
+        self.vis = vis
+        self.rfi_run = rfi_run
+        self.time_step = time_step
+
+    def name(self):
+        """ Name of the Sink Provider """
+        return self.__class__.__name__
+
+    def model_vis(self, context):
+        """ Receive model visibilities from Montblanc in `context.data` """
+        extents = context.array_extents(context.name)
+        (lt, ut), (lbl, ubl), (lc, uc), (lp, up) = extents
+        # noise = context.data/20
+        # complex_noise = np.random.randn(*context.shape) * noise + \
+                        # np.random.randn(*context.shape) * noise * 1j
+        if self.rfi_run:
+            i = self.time_step
+            self.vis[i, lbl:ubl, lc:uc, lp:up] = context.data# + complex_noise
+        else:
+            self.vis[lt:ut, lbl:ubl, lc:uc, lp:up] = context.data# + complex_noise
--- RFIsim.py
+++ RFIsim.py
@@ -12,6 +12,7 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 # Internal imports
+from RFIProviders import RFISourceProvider, RFISinkProvider
 from utils.uv_sim.uvgen import UVCreate
 from utils.sat_sim.sim_sat_paths import get_lm_tracks, radec_to_lm
 from utils.sat_sim.sim_sat_spectra import get_sat_spectra
@@ -19,175 +20,175 @@ from utils.bandpass.bandpass_gains import get_bandpass_and_gains
 from utils.catalogues.get_ast_sources import inview
 from utils.write_to_h5 import save_output, save_input
 
-######## Source Provider #######################################################
-
-class RFISourceProvider(SourceProvider):
-    """
-    Supplies data to montblanc via data source methods,
-    which have the following signature.
-
-    .. code-block:: python
-
-        def point_lm(self, context)
-            ...
-    """
-    def name(self):
-        """ Name of Source Provider """
-        return self.__class__.__name__
-
-    def updated_dimensions(self):
-        """ Inform montblanc about dimension sizes """
-        if j==0:
-            return [("ntime", ntime),              # Timesteps
-                    ("nchan", nchan),              # Channels
-                    ("na", n_ant),                    # Antenna
-                    ("nbl", n_ant*(n_ant-1)/2),          # Baselines
-                    ("npsrc", len(lm_coords)),     # Number of point sources
-                    ("ngsrc", 0)]                  # Number of gaussian sources
-                    # ("ngsrc", len(gauss_sources))] # Number of gaussian sources
-        else:
-            return [("ntime", time_steps),         # Timesteps
-                    ("nchan", nchan),              # Channels
-                    ("na", n_ant),                    # Antenna
-                    ("nbl", n_ant*(n_ant-1)/2),          # Baselines
-                    ("npsrc", 0),                  # Number of point sources
-                    ("ngsrc", len(gauss_sources))] # Number of gaussian sources
-
-    def point_lm(self, context):
-        """ Supply point source lm coordinates to montblanc """
-
-        # Shape (npsrc, 2)
-        (ls, us), _ = context.array_extents(context.name)
-
-        return np.asarray(lm_coords, dtype=context.dtype)[ls:us, :]
-
-    def point_stokes(self, context):
-        extents = context.dim_extents('npsrc', 'ntime', 'nchan')
-        (lp, up), (lt, ut), (lc, uc) = extents
-        # (npsrc, ntime, nchan, 4)
-
-        spec = np.ones((1, time_steps, 1, 1))
-        spec = spectra*spec
-        return spec[lp:up, lt:ut, lc:uc, :]
-
-######## New ############################################################################
-
-    def gaussian_lm(self, context):
-        """ Returns an lm coordinate array to Montblanc. """
-
-        # lm = np.empty(context.shape, context.dtype)
-        lm = np.empty((len(gauss_sources), 2), context.dtype)
-
-        # Get the extents of the time, baseline and chan dimension
-        (lg, ug) = context.dim_extents('ngsrc')
-
-        ra = gauss_sources['RA']
-        dec = gauss_sources['DEC']
-        phase_centre = [target_ra, target_dec]
-
-        lm[:,0], lm[:,1] = radec_to_lm(ra, dec, phase_centre)
-
-        return lm[lg:ug]
-
-    def gaussian_shape(self, context):
-        """ Returns a Gaussian shape array to Montblanc """
-
-        (lg, ug) = context.dim_extents('ngsrc')
-
-        emaj = np.deg2rad(gauss_sources['MajAxis'].values)
-        emin = np.deg2rad(gauss_sources['MinAxis'].values)
-        pa = np.deg2rad(gauss_sources['PA'].values)
-
-        # gauss = np.empty(context.shape, dtype=context.dtype)
-        gauss = np.empty((3, len(gauss_sources)), context.dtype)
-
-        gauss[0,:] = emaj * np.sin(pa)
-        gauss[1,:] = emaj * np.cos(pa)
-        emaj[emaj == 0.0] = 1.0
-        gauss[2,:] = emin / emaj
-
-        return gauss[:,lg:ug]
-
-    def gaussian_stokes(self, context):
-        """ Return a stokes parameter array to Montblanc """
-
-        # Get the extents of the time, baseline and chan dimension
-        extents = context.dim_extents('ngsrc', 'ntime', 'nchan')
-        (lg, ug), (lt, ut), (lc, uc) = extents
-        print(context.shape)
-        # stokes = np.empty(context.shape, context.dtype)
-        stokes = np.empty((len(gauss_sources), 1, uc-lc, 4), context.dtype)
-
-        stokes[:,:,:,0] = gauss_sources['Flux'].values[:,None,None]
-        stokes[:,:,:,1:] = 0.0
-
-        return stokes[lg:ug]
-
-######################################################################################
-
-    # def direction_independent_effects(self, context):
-    #     # (ntime, na, nchan, npol)
-    #     extents = context.dim_extents('ntime', 'na', 'nchan', 'npol')
-    #     (lt, ut), (la, ua), (lc, uc), (lp, up) = extents
-    #
-    #     bp = np.ones((time_steps, 1, 1, 1))
-    #     bp = bandpass*bp
-    #     return bp[lt:ut, la:ua, lc:uc, lp:up]
-
-    def uvw(self, context):
-        """ Supply UVW antenna coordinates to montblanc """
-
-        # Shape (ntime, na, 3)
-        (lt, ut), (la, ua), (l, u) = context.array_extents(context.name)
-
-        if j==0:
-            idx = np.arange(i*2016, (i+1)*2016)
-            auvw = mbu.antenna_uvw(UVW[idx], A1, A2, np.array([2016,]),
-                                   nr_of_antenna=n_ant)
-        else:
-            AA1 = []
-            AA2 = []
-            for _ in range(UVW.shape[0]/2016):
-                AA1 += list(A1)
-                AA2 += list(A2)
-            AA1 = np.array(AA1)
-            AA2 = np.array(AA2)
-            chunks = np.array(UVW.shape[0]/2016*[n_ant*(n_ant-1)/2], dtype=np.int)
-            auvw = mbu.antenna_uvw(UVW, AA1, AA2, chunks, nr_of_antenna=n_ant)
-
-        return auvw[lt:ut, la:ua, l:u]
-
-######### Sink Provider ########################################################
-
-class RFISinkProvider(SinkProvider):
-    """
-    Receives data from montblanc via data sink methods,
-    which have the following signature
-
-    .. code-block:: python
-
-        def model_vis(self, context):
-            print context. data
-    """
-    def name(self):
-        """ Name of the Sink Provider """
-        return self.__class__.__name__
-
-    def model_vis(self, context):
-        """ Receive model visibilities from Montblanc in `context.data` """
-        extents = context.array_extents(context.name)
-        (lt, ut), (lbl, ubl), (lc, uc), (lp, up) = extents
-        global vis
-        # noise = context.data/20
-        context_shape = (ut-lt, ubl-lbl, uc-lc, up-lp)
-        # complex_noise = np.random.randn(*context_shape) * noise + \
-                        # np.random.randn(*context_shape) * noise * 1j
-        if j==0:
-            vis[i, lbl:ubl, lc:uc, lp:up] = context.data# + complex_noise
-        else:
-            vis[lt:ut, lbl:ubl, lc:uc, lp:up] = context.data# + complex_noise
-
-        return vis
+# ######## Source Provider #######################################################
+#
+# class RFISourceProvider(SourceProvider):
+#     """
+#     Supplies data to montblanc via data source methods,
+#     which have the following signature.
+#
+#     .. code-block:: python
+#
+#         def point_lm(self, context)
+#             ...
+#     """
+#     def name(self):
+#         """ Name of Source Provider """
+#         return self.__class__.__name__
+#
+#     def updated_dimensions(self):
+#         """ Inform montblanc about dimension sizes """
+#         if j==0:
+#             return [("ntime", ntime),              # Timesteps
+#                     ("nchan", nchan),              # Channels
+#                     ("na", n_ant),                    # Antenna
+#                     ("nbl", n_ant*(n_ant-1)/2),          # Baselines
+#                     ("npsrc", len(lm_coords)),     # Number of point sources
+#                     ("ngsrc", 0)]                  # Number of gaussian sources
+#                     # ("ngsrc", len(gauss_sources))] # Number of gaussian sources
+#         else:
+#             return [("ntime", time_steps),         # Timesteps
+#                     ("nchan", nchan),              # Channels
+#                     ("na", n_ant),                    # Antenna
+#                     ("nbl", n_ant*(n_ant-1)/2),          # Baselines
+#                     ("npsrc", 0),                  # Number of point sources
+#                     ("ngsrc", len(gauss_sources))] # Number of gaussian sources
+#
+#     def point_lm(self, context):
+#         """ Supply point source lm coordinates to montblanc """
+#
+#         # Shape (npsrc, 2)
+#         (ls, us), _ = context.array_extents(context.name)
+#
+#         return np.asarray(lm_coords, dtype=context.dtype)[ls:us, :]
+#
+#     def point_stokes(self, context):
+#         extents = context.dim_extents('npsrc', 'ntime', 'nchan')
+#         (lp, up), (lt, ut), (lc, uc) = extents
+#         # (npsrc, ntime, nchan, 4)
+#
+#         spec = np.ones((1, time_steps, 1, 1))
+#         spec = spectra*spec
+#         return spec[lp:up, lt:ut, lc:uc, :]
+#
+# ######## New ############################################################################
+#
+#     def gaussian_lm(self, context):
+#         """ Returns an lm coordinate array to Montblanc. """
+#
+#         # lm = np.empty(context.shape, context.dtype)
+#         lm = np.empty((len(gauss_sources), 2), context.dtype)
+#
+#         # Get the extents of the time, baseline and chan dimension
+#         (lg, ug) = context.dim_extents('ngsrc')
+#
+#         ra = gauss_sources['RA']
+#         dec = gauss_sources['DEC']
+#         phase_centre = [target_ra, target_dec]
+#
+#         lm[:,0], lm[:,1] = radec_to_lm(ra, dec, phase_centre)
+#
+#         return lm[lg:ug]
+#
+#     def gaussian_shape(self, context):
+#         """ Returns a Gaussian shape array to Montblanc """
+#
+#         (lg, ug) = context.dim_extents('ngsrc')
+#
+#         emaj = np.deg2rad(gauss_sources['MajAxis'].values)
+#         emin = np.deg2rad(gauss_sources['MinAxis'].values)
+#         pa = np.deg2rad(gauss_sources['PA'].values)
+#
+#         # gauss = np.empty(context.shape, dtype=context.dtype)
+#         gauss = np.empty((3, len(gauss_sources)), context.dtype)
+#
+#         gauss[0,:] = emaj * np.sin(pa)
+#         gauss[1,:] = emaj * np.cos(pa)
+#         emaj[emaj == 0.0] = 1.0
+#         gauss[2,:] = emin / emaj
+#
+#         return gauss[:,lg:ug]
+#
+#     def gaussian_stokes(self, context):
+#         """ Return a stokes parameter array to Montblanc """
+#
+#         # Get the extents of the time, baseline and chan dimension
+#         extents = context.dim_extents('ngsrc', 'ntime', 'nchan')
+#         (lg, ug), (lt, ut), (lc, uc) = extents
+#         print(context.shape)
+#         # stokes = np.empty(context.shape, context.dtype)
+#         stokes = np.empty((len(gauss_sources), 1, uc-lc, 4), context.dtype)
+#
+#         stokes[:,:,:,0] = gauss_sources['Flux'].values[:,None,None]
+#         stokes[:,:,:,1:] = 0.0
+#
+#         return stokes[lg:ug]
+#
+# ######################################################################################
+#
+#     # def direction_independent_effects(self, context):
+#     #     # (ntime, na, nchan, npol)
+#     #     extents = context.dim_extents('ntime', 'na', 'nchan', 'npol')
+#     #     (lt, ut), (la, ua), (lc, uc), (lp, up) = extents
+#     #
+#     #     bp = np.ones((time_steps, 1, 1, 1))
+#     #     bp = bandpass*bp
+#     #     return bp[lt:ut, la:ua, lc:uc, lp:up]
+#
+#     def uvw(self, context):
+#         """ Supply UVW antenna coordinates to montblanc """
+#
+#         # Shape (ntime, na, 3)
+#         (lt, ut), (la, ua), (l, u) = context.array_extents(context.name)
+#
+#         if j==0:
+#             idx = np.arange(i*2016, (i+1)*2016)
+#             auvw = mbu.antenna_uvw(UVW[idx], A1, A2, np.array([2016,]),
+#                                    nr_of_antenna=n_ant)
+#         else:
+#             AA1 = []
+#             AA2 = []
+#             for _ in range(UVW.shape[0]/2016):
+#                 AA1 += list(A1)
+#                 AA2 += list(A2)
+#             AA1 = np.array(AA1)
+#             AA2 = np.array(AA2)
+#             chunks = np.array(UVW.shape[0]/2016*[n_ant*(n_ant-1)/2], dtype=np.int)
+#             auvw = mbu.antenna_uvw(UVW, AA1, AA2, chunks, nr_of_antenna=n_ant)
+#
+#         return auvw[lt:ut, la:ua, l:u]
+#
+# ######### Sink Provider ########################################################
+#
+# class RFISinkProvider(SinkProvider):
+#     """
+#     Receives data from montblanc via data sink methods,
+#     which have the following signature
+#
+#     .. code-block:: python
+#
+#         def model_vis(self, context):
+#             print context. data
+#     """
+#     def name(self):
+#         """ Name of the Sink Provider """
+#         return self.__class__.__name__
+#
+#     def model_vis(self, context):
+#         """ Receive model visibilities from Montblanc in `context.data` """
+#         extents = context.array_extents(context.name)
+#         (lt, ut), (lbl, ubl), (lc, uc), (lp, up) = extents
+#         global vis
+#         # noise = context.data/20
+#         context_shape = (ut-lt, ubl-lbl, uc-lc, up-lp)
+#         # complex_noise = np.random.randn(*context_shape) * noise + \
+#                         # np.random.randn(*context_shape) * noise * 1j
+#         if j==0:
+#             vis[i, lbl:ubl, lc:uc, lp:up] = context.data# + complex_noise
+#         else:
+#             vis[lt:ut, lbl:ubl, lc:uc, lp:up] = context.data# + complex_noise
+#
+#         return vis
 
 ########## Configuration #######################################################
 
@@ -205,11 +206,29 @@ def call_solver():
 
         FITSfiles = 'utils/beam_sim/beams/FAKE_$(corr)_$(reim).fits'
 
+        # vis = vis
+        rfi_run = bool((j+1)%2)
+        n_time = ntime
+        n_chan = nchan
+        # n_ant = n_ant
+        # bandpass = bandpass
+        # gauss_sources = gauss_sources
+        p_spectrum = spectra
+        rfi_lm = lm_coords
+        # phase_centre = phase_centre
+        time_step = i
+        # A1 = A1
+        # A2 = A2
+        # UVW = UVW
+
+        source = RFISourceProvider(rfi_run, n_time, n_chan, n_ant,
+                                  bandpass, gauss_sources, p_spectrum,
+                                  rfi_lm, phase_centre, time_step, A1, A2, UVW)
         # Create RFI Source and Sink Providers
-        source_provs = [RFISourceProvider(),
+        source_provs = [source
                         # FitsBeamSourceProvider(FITSfiles)
                        ]
-        sink_provs = [RFISinkProvider()]
+        sink_provs = [RFISinkProvider(vis, rfi_run, time_step)]
 
         # Call solver, supplying source and sink providers
         slvr.solve(source_providers=source_provs,
@@ -253,7 +272,9 @@ gauss_sources = inview(phase_centre, radius=10, min_flux=0.5)
 #### Get lm tracks of satellites ##### lm shape (time_steps, vis_sats+1, 2) ####
 lm = get_lm_tracks(phase_centre, transit, tracking_hours,
                    integration_secs)
-time_steps, n_sats = lm.shape[:2]
+print(lm.shape)
+time_steps = lm.shape[0]
+n_sats = lm.shape[1]-1
 
 ###### Get satellite spectra ###################################################
 
@@ -285,6 +306,7 @@ for j in range(2):
                           dtype=np.complex128)
 
     if j==1:
+        ntime = time_steps
         call_solver()
     else:
         for i in range(len(lm)):
